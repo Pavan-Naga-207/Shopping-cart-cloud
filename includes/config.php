201473d4f77739dd3fe8cee05eac2bf16b2dfc75
Pavan-Naga-207 <109537572+Pavan-Naga-207@users.noreply.github.com>
2023-12-02 22:05:01 +0530

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -1,9 +1,15 @@
 <?php
-define('DB_SERVER','localhost');
-define('DB_USER','root');
-define('DB_PASS' ,'');
+define('DB_SERVER','shoppingdbserver.mysql.database.azure.com');
+define('DB_USER','database');
+define('DB_PASS' ,'pass@1234');
 define('DB_NAME', 'shopping');
-$con = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
+$host = 'shoppingdbserver.mysql.database.azure.com';
+$username = 'database';
+$password = 'pass@1234';
+$db_name = 'shopping';
+$conn = mysqli_init();
+mysqli_ssl_set($conn,NULL,NULL, "DigiCertGlobalRootCA.crt.pem", NULL, NULL);
+mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306, NULL, MYSQLI_CLIENT_SSL);
 // Check connection
 if (mysqli_connect_errno())
 {

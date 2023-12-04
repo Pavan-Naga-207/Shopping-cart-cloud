a76492676ab8f0215857148606fe42c2fd4a9a4e
Pavan-Naga-207 <109537572+Pavan-Naga-207@users.noreply.github.com>
2023-12-04 11:18:46 +0530

    Update login.php

--- login.php
+++ login.php
@@ -25,7 +25,7 @@ if(isset($_POST['login']))
    $password=md5($_POST['password']);
    $data=array('email'=>$email,'password'=>$password);
    
-   $azfendpoint='https://authenticate4.azurewebsites.net/api/http_trigger?code=xaCRw_8vTx9bBh2HcQPQOziQ9LmV7a9_FcrnzgCJt6MUAzFuGQ4BxQ=='
+   $azfendpoint='https://authenticate44.azurewebsites.net/api/HttpTrigger1?code=Gi4dJV9ZTBStlZjmL9a8N6l1Nwbx9VfauNh-DabAlsPOAzFuJQp0pg=='
    $options = array(
 	'http' => array(
 		'method' => 'POST',
